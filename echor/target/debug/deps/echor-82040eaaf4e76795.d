5cec5c315b6ce25704433373d0ba882efb75378b
Ryan Ellis <ryan.ellis33@gmail.com>
2023-02-11 16:55:57 -0700

    Initial Commit

---
+++ echor/target/debug/deps/echor-82040eaaf4e76795.d
@@ -0,0 +1,5 @@
+/home/smebellis/rust-solutions/echor/target/debug/deps/echor-82040eaaf4e76795: src/main.rs
+
+/home/smebellis/rust-solutions/echor/target/debug/deps/echor-82040eaaf4e76795.d: src/main.rs
+
+src/main.rs:
---
+++ echor/target/debug/echor.d
@@ -0,0 +1,1 @@
+/home/smebellis/rust-solutions/echor/target/debug/echor: /home/smebellis/rust-solutions/echor/src/main.rs
